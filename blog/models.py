388ebdd61dbfb4e7ec0effd886392e63152488f9
AadamBari <aadambari@msn.com>
2020-01-24 03:00:42 +0000

    added model for blog post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+# declared a tuple for STATUS of a post to keep draft and published posts separated when we render them out with templates.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+        # sort results in the created_on field in descending order by default when querying the database
+        # specify descending order using the negative prefix
+        # so posts published recently will appear first
+
+    def __str__(self):
+        return self.title
