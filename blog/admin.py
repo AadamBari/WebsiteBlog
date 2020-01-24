cb2c704876a175b03629cd349ef52bdb5d3bd8f1
AadamBari <aadambari@msn.com>
2020-01-24 03:01:44 +0000

    customised admin page options of post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post 
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status",) # on admin page gives option to filter posts by status (draft/publish)
+    search_fields = ['title', 'content'] # search terms for search bar options 
+    prepopulated_fields = {'slug': ('title',)} # slug field will be automatically populated based on title
+  
+admin.site.register(Post, PostAdmin)
